85e0089269c50751e7a8f2c6f4d53f73d95d2438
Mohammad Zaid <mohdzaid07091997@gmail.com>
2025-08-12 10:20:07 +0530

    ENV Added

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const app = express();
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
+const db_host = process.env.DB_HOST;
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+const db_name = process.env.DB_NAME;
 
 // Configure your MySQL connection here
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'MYSQL@zaid17',
-    database: 'schools_db',
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_name,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
